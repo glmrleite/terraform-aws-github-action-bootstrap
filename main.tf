021dd0418ae91e2ba4de750be811706cfe22fa7e
Gilmar Leite <gilmarsilva1@gmail.com>
2022-02-03 11:35:23 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
- terraform {
-   backend "s3" {
-     bucket = "example-terraform-project-name-bootstrap-terraform-state-gilmar"
-     key    = "default-infrastructure"
-     region = "us-east-1"
-   }
- }
-
- resource "aws_s3_bucket" "terraform_state" {
-   bucket = "example-terraform-project-name-bootstrap-terraform-state-gilmar"
-
-   versioning {
-     enabled = true
-   }
- }
+resource "aws_s3_bucket" "b" {
+  bucket = "teste-s3-githubactions-gilmar"
+  acl    = "private"
+}
